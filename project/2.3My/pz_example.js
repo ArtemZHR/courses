6f5ffedf31d6b4a780a24cecd97ca79c2b8e60da
Artem <artem.zhiruk@hotmail.com>
2020-05-01 16:18:31 +0300

    Сделал восьмое задание регуляркой

--- project/2.3My/pz_example.js
+++ project/2.3My/pz_example.js
@@ -139,75 +139,76 @@
 
 //ВОСЬМОЕ ЗАДАНИЕ
 
-let text = prompt("Введите ваш текст");
-let lText = text.toLowerCase();
-let sTwo = "";
-let marks = ",.!?-–;<>[]()@#$%^&* \r\n"; //Строка, для поиска символов препинания и символов которые не относяться к словам
-let arrayWords = []; //Создаем массив для того, чтобы заносить в него слова
-let tempString = ""; //Строка, необходимая для хранения слова из строки
-let countWords = 0; //Счетчик массива ArrayWords
+// let text = prompt("Введите ваш текст");
+// let lText = text.toLowerCase();
+// let sTwo = "";
+// let marks = ",.!?-–;<>[]()@#$%^&* \r\n"; //Строка, для поиска символов препинания и символов которые не относяться к словам
+// let arrayWords = []; //Создаем массив для того, чтобы заносить в него слова
+// let tempString = ""; //Строка, необходимая для хранения слова из строки
+// let countWords = 0; //Счетчик массива ArrayWords
 
 
-for (var i = 0; i < lText.length; i++) { //Разбиваем на слова, работает до нахождение распределителя "marks"
-  if (marks.indexOf(lText[i]) == -1) { //Записываем буквы для создание слова если находит распределитель идет в else
-    tempString += lText[i];
-  } else {
-    if (tempString != '') {
-      arrayWords[countWords] = tempString; //Записываем слово в массив
-      countWords++;
-    }
-    tempString = ""; //Обнуляет 
-  }
-}
+// for (var i = 0; i < lText.length; i++) { //Разбиваем на слова, работает до нахождение распределителя "marks"
+//   if (marks.indexOf(lText[i]) == -1) { //Записываем буквы для создание слова если находит распределитель идет в else
+//     tempString += lText[i];
+//   } else {
+//     if (tempString != '') {
+//       arrayWords[countWords] = tempString; //Записываем слово в массив
+//       countWords++;
+//     }
+//     tempString = ""; //Обнуляет 
+//   }
+// }
 
-//ПЕРВЫЙ СПОСОБ
+// //ПЕРВЫЙ СПОСОБ
 
-let ks=[];//массив значений
-for(let value of arrayWords){ 
-    ks[value] = (ks[value]||0)+1; //Записываем сколько раз значение повторяеться
-}
-let arr2 =[];
-for(var i in arrayWords) {
-  if ( ks[ arrayWords[i] ] > 1 ) {//Ищем повторяющейся значения
-        arr2.push(arrayWords[i]);//Записываем повторяющейся значения
-    }
-}
-arr2.sort();
+// let ks=[];//массив значений
+// for(let value of arrayWords){ 
+//     ks[value] = (ks[value]||0)+1; //Записываем сколько раз значение повторяеться
+// }
+// let arr2 =[];
+// for(var i in arrayWords) {
+//   if ( ks[ arrayWords[i] ] > 1 ) {//Ищем повторяющейся значения
+//         arr2.push(arrayWords[i]);//Записываем повторяющейся значения
+//     }
+// }
+// arr2.sort();
 
-let result = {};
-arr2.forEach(function(a){ //Записываем сколько раз конкретное значение повторяеться
-    result[a] = result[a] + 1 || 1;
-});
-for (var key in result){ //Вывод слов которые повторяються и сколько раз
-    console.log('Слово: ' + key + ' = ' + result[key] + ' раз(а)');
-}
-let arr3 = Array.from(new Set(arr2));//Убираем одинаковые значения и оставляем уникальные 
-console.log("Количество слов которые повторяються: " + arr3.length);//Показать сколько слов в тексте повторяеться
+// let result = {};
+// arr2.forEach(function(a){ //Записываем сколько раз конкретное значение повторяеться
+//     result[a] = result[a] + 1 || 1;
+// });
+// for (var key in result){ //Вывод слов которые повторяються и сколько раз
+//     console.log('Слово: ' + key + ' = ' + result[key] + ' раз(а)');
+// }
+// let arr3 = Array.from(new Set(arr2));//Убираем одинаковые значения и оставляем уникальные 
+// console.log("Количество слов которые повторяються: " + arr3.length);//Показать сколько слов в тексте повторяеться
 
 //ЗДЕСЬ ДОЛЖНА БЫТЬ ВЫБОРКА СЛОВ РЕГУЛЯРКОЙ
-// let text = prompt("Введите ваш текст");
-// let text1 = text.toLowerCase();
-// let req = /[^\s]+/;
-// let arrayWords = [];
-// arrayWords = text.match(req);
+let text = prompt("Введите ваш текст");
+let text1 = text.toLowerCase();
+let req = /[^\s\w$,.\s\r\n]+/ig;
+let arrayWords = text1.match(req);
+
 // for (let i = 0; i < text.length; i++) {
 //   console.log(arrayWords[i]);
+
 // }
 
 //ВРОТОЙ СПОСОБ
 
-// let counter = {}; //записываем слово и количество повторов
-// arrayWords.forEach(word => {
-//   if (counter[word]) {
-//     counter[word] += 1; //если слово  встречалось ключ к нему +1
-//     return;
-//   } else {
-//     counter[word] = 1; //если слово ещё не встречалось ключ к нему =1
-//   }
-// });
-// for (let i of Object.keys(counter)) {
-//   if (counter[i] > 1) { //вывод слов в которых ключ(количество повторений) больше 1
-//     console.log("Слово: \"" + i + "\" повторилось в тексте  \"" + counter[i] + "\" раз(a).");
-//   }
+let counter = {}; //записываем слово и количество повторов
+arrayWords.forEach(word => {
+  if (counter[word]) {
+    counter[word] += 1; //если слово  встречалось ключ к нему +1
+    return;
+  } else {
+    counter[word] = 1; //если слово ещё не встречалось ключ к нему =1
+  }
+});
+for (let i of Object.keys(counter)) {
+  if (counter[i] > 1) { //вывод слов в которых ключ(количество повторений) больше 1
+    console.log("Слово: \"" + i + "\" повторилось в тексте  \"" + counter[i] + "\" раз(a).");
+  }
 
-// }
+}
